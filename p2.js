ab06bc79c8d9c0533f96b2cfdd865becb535d4ef
Yasmeen2006 <nasseryasmeen457@gmail.com>
2025-11-22 16:13:31 +0200

    first commit

---
+++ p2.js
@@ -0,0 +1,76 @@
+// problem 2 
+
+// function flick(arr){
+//   let always=true
+//   let str=[]
+//   for(let item of arr){
+//     if (item==='flick'){
+//       always =!always}
+   
+//    str.push(always)
+//   }
+//   return str
+// }
+// console.log(flick(['codewars', 'flick', 'code', 'wars']))
+// console.log(flick(['flick', 'chocolate', 'adventure', 'sunshine']))
+// console.log(flick(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']))
+
+
+//problem 3
+// for(let i=1; i<=12 ; i++) 
+//   {
+//   if (i===1|| i===2 || i===3 )
+//     console.log(i+' is part of the first quarter')
+//   else if (i===4|| i===5||i===6)
+//     console.log(i+' is part of the second quarter')
+//     else if (i===7|| i===8||i===9)
+//     console.log(i+' is part of the third quarter')
+//     else if (i===10|| i===11||i===12)
+//     console.log(i+' is part of the fourth quarter')
+// }
+
+//  problem 4
+// let arr=['sheep', 'sheep', 'wolf']
+// // let arr1=['sheep', 'sheep', 'sheep', 'wolf', 'sheep'] test 2 
+// let rev = arr.reverse();
+
+// let pw = rev.indexOf('wolf')
+
+//  if (pw===0)
+//  {
+//   console.log('Pls go away and stop eating my sheep')
+//  }
+//  else
+//  {
+//   console.log(`Oi! Sheep number ${pw}You are about to be eaten by a wolf!`)
+//  }
+
+// problem 5 
+
+// function tPoint(game){
+//     let tot = 0;
+//     for (let i = 0; i < game.length; i++) {
+//         let mat = game[i];
+//         let f = mat[0];  //first score
+//         let s = mat[2];  //second score
+//         if (f > s) tot += 3;
+//         else if (f === s) tot += 1;
+//     }
+    
+//     return tot;
+// }
+
+
+// problem 6
+// function sumOfDifferences(arr) {
+//     return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+// }
+
+
+//problem 7
+
+function fGrade(exam, project) {
+return exam > 90 || project > 10 ? 100 :
+  exam > 75 || project >= 5 ? 90 :
+  exam > 50  || project >= 2 ?  75 : 0 
+}
